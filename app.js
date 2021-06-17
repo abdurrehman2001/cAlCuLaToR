18a5fbc4a69190eb302d93c68507c3e5d31b3417
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-06-17 18:30:19 +0500

    Done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getNumber(num){
     var reslt = document.getElementById('digit')
     var lstnum = reslt.value.slice(reslt.value.length - 1)
-    var oprs = ["+" , "-" , "*" , "/"]
+    var oprs = ["+" , "-" , "*" , "/" , "."]
     if(oprs.indexOf(lstnum) !== -1 && oprs.indexOf(num) !== -1){
         reslt.value = reslt.value.slice(0 , reslt.value.length - 1) + num
 
